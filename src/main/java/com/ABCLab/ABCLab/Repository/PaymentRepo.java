3ac25d2c6042d2503424735452014c1ee06c869c
Thevindu Vithanage <97927940+Thiyasara-github@users.noreply.github.com>
2024-03-12 11:36:24 +0530

    creating PaymentRepo in repository

---
+++ src/main/java/com/ABCLab/ABCLab/Repository/PaymentRepo.java
@@ -0,0 +1,7 @@
+package com.ABCLab.ABCLab.Repository;
+
+import com.ABCLab.ABCLab.Model.Payment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PaymentRepo extends MongoRepository<Payment, Integer> {
+}
