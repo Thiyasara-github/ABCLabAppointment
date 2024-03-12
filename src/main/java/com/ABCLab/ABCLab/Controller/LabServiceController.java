4c7a224f5a590f61df685dbe163774495fefccb5
Thevindu Vithanage <97927940+Thiyasara-github@users.noreply.github.com>
2024-03-12 11:02:23 +0530

    cross origin for labservice controller

--- src/main/java/com/ABCLab/ABCLab/Controller/LabServiceController.java
+++ src/main/java/com/ABCLab/ABCLab/Controller/LabServiceController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:63342")
 public class LabServiceController {
 
     @Autowired
